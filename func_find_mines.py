e23a240fabebd9d6889eceb142133b9c09b3b336
arysttoto <105072580+arysttoto@users.noreply.github.com>
2023-05-02 19:17:29 +0600

    Update func_find_mines.py

--- func_find_mines.py
+++ func_find_mines.py
@@ -1,37 +1,11 @@
 # finding neighbouring mines using try and except methods
-def find_mines(buttons, x, y):
+def find_mines(buttons, x, y, rows, cols):
     # calculating the number of mines around by increment algorithm, using try and except blocks to make sure we don't call wrong indexes
     c = 0
-    try:
-        if buttons[x + 1][y].mine:
-            c += 1
-    except:
-        pass
-    if x - 1 != -1 and buttons[x - 1][y].mine:
-        c += 1
-    try:
-        if buttons[x][y + 1].mine:
-            c += 1
-    except:
-        pass
-    if y - 1 != -1 and buttons[x][y - 1].mine:
-        c += 1
-    try:
-        if buttons[x + 1][y + 1].mine:
-            c += 1
-    except:
-        pass
-    try:
-        if y - 1 != -1 and buttons[x + 1][y - 1].mine:
-            c += 1
-    except:
-        pass
-    if x - 1 != -1 and y - 1 != -1 and buttons[x - 1][y - 1].mine:
-        c += 1
-    try:
-        if x - 1 != -1 and buttons[x - 1][y + 1].mine:
-            c += 1
-    except:
-        pass
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            if (x + i <= rows - 1) and (y + j <= cols - 1) and (y + j >= 0) and (x + i >= 0):
+                if buttons[x + i][y + j].mine:
+                    c += 1
 
-    return c
+    return c 
